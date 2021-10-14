e77da17354fc5c0c1973e211f3f8585a9235b4ad
BranoAsaya <87427429+BranoAsaya@users.noreply.github.com>
2021-10-14 17:17:16 +0300

    first commit

---
+++ NodeWebServer/HTTPserver.js
@@ -0,0 +1,91 @@
+console.log("loading..");
+const http = require("http");
+const fs = require("fs");
+
+const products = [
+  {
+    name: "jo",
+    price: 125,
+    img: "https://s.alicdn.com/@sc04/kf/HTB1GTBVKv5TBuNjSspmq6yDRVXar.jpg_220x220.jpg",
+  },
+  {
+    name: "go",
+    price: 155,
+    img: "https://s.alicdn.com/@sc04/kf/H850446eb34b147a2a83acc8157365bc3y.jpg_220x220.jpg",
+  },
+  {
+    name: "dc",
+    price: 127,
+    img: "https://s.alicdn.com/@sc04/kf/HTB1vkN.EDJYBeNjy1zeq6yhzVXa8.jpg_220x220.jpg",
+  },
+  {
+    name: "er",
+    price: 145,
+    img: "https://s.alicdn.com/@sc04/kf/HTB1GTBVKv5TBuNjSspmq6yDRVXar.jpg_220x220.jpg",
+  },
+  {
+    name: "po",
+    price: 175,
+    img: "https://s.alicdn.com/@sc04/kf/HTB1GTBVKv5TBuNjSspmq6yDRVXar.jpg_220x220.jpg",
+  },
+];
+const productsJson=JSON.stringify(products)
+console.log(productsJson);
+const product = {
+  name: "up",
+  price: 111,
+  img: "https://s.alicdn.com/@sc04/kf/HTB1Pi_4LXXXXXb2XpXXq6xXFXXX6.jpg_220x220.jpg",
+};
+const productJson=JSON.stringify(product)
+
+const server = http.createServer((req, res) => {
+  switch (req.url) {
+    case "/":
+      fs.readFile("public/index.html", "utf8", (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("wrong");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+    case "/about":
+      fs.readFile("public/about.html", "utf8", (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("wrong");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+    case "/sales":
+      fs.readFile("public/sales.html", "utf8", (err, data) => {
+        if (err) {
+          res.writeHead(404);
+          res.write("wrong");
+        } else {
+          res.write(data);
+        }
+        res.end();
+      });
+      break;
+    case "/products":
+      res.write(productsJson);
+      res.end();
+
+      break;
+    case "/product":
+      res.write(productJson);
+      res.end();
+      break;
+    default:
+      res.writeHead(404);
+      res.write("wrong");
+      break;
+  }
+});
+server.listen(2000);
